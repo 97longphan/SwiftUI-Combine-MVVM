30c83d34b097b966e6af57a3806106ca724c34bf
Long Mango Phan Hoang <phlong.cmc@pomelofashion.com>
2022-04-07 16:09:38 +0700

    First init

---
+++ SwiftUI-Combine-MVVM/SwiftUI_Combine_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Combine_MVVMApp.swift
+//  SwiftUI-Combine-MVVM
+//
+//  Created by nxnam on 07/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Combine_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
